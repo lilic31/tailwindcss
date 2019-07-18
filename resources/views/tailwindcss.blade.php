833130ca069451439f4b415672ae993479b5f39f
lilic31 <alibert.eric@gmail.com>
2019-07-18 08:12:30 +0200

    Init page connexion

---
+++ resources/views/tailwindcss.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+    <title>Test de tailwind</title>
+</head>
+<body>
+
+    <div class="flex justify-center items-center min-h-screen bg-gray-100">
+
+
+        <div class="bg-white shadow-md max-w-lg">
+            <img class="w-full h-40 object-cover" src="https://picsum.photos/id/1047/200/300" alt="">
+            <h1 class="text-lg">
+                Une jolie boîte de dialogue
+            </h1>
+        </div>
+
+    </div>
+
+
+</body>
+</html>
